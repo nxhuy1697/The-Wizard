a26cd23b5f4558046a3c5ffce7600cd27e2dd4ea
nxhuy1697 <nxhuy1697@gmail.com>
2025-01-06 10:40:55 +0700

    Initial commit

---
+++ src/components/FireFliesBackground.jsx
@@ -0,0 +1,45 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const createFireFly = () => ({
+  id: Math.random(),
+  top: `${Math.random()*100}%`,
+  left: `${Math.random()*100}%`,
+  animationDuration: `${Math.random()*5 + 5}s`,
+});
+
+const FireFliesBackground = () => {
+  const [fireFlies, setFireFlies] = useState([]);
+
+  useEffect(() => {
+    const addFireFlyPeriodically = () => {
+      const newFireFly = createFireFly();
+      setFireFlies(currentFireflies => [
+        ...currentFireflies.slice(-14),
+        newFireFly
+      ] );
+    };
+    const interval = setInterval( addFireFlyPeriodically, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-full -z-10 overflow-hidden">
+      {fireFlies.map((fireFly) => {
+        return (
+          <div
+            key={fireFly.id}
+            className="absolute rounded-full w-[10px] h-[10px] bg-firefly-radial "
+            style={{
+                 top: fireFly.top,
+                 left: fireFly.left,
+                 animation: `move ${fireFly.animationDuration} infinite alternate`,
+                 }}
+          ></div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FireFliesBackground;
